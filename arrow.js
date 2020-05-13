ffb5fe41a7ce493b2604cfee7527e022d78e8422
BbillahrariBBr <mb.billah8@gmail.com>
2020-05-13 11:46:28 +0600

    add arrow function

---
+++ arrow.js
@@ -0,0 +1,10 @@
+// to write smaller function
+// cannot use constructor, like a anonymus function
+
+// declare arrow function
+
+var myArrow = () =>{
+    console.log("Myfirst arrow function")
+}
+myArrow();
+//not working git
