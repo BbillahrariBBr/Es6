fce2496d2b6c933c6f61fccebd50b11ac6fae623
BbillahrariBBr <mb.billah8@gmail.com>
2020-05-13 11:53:32 +0600

    simple Array

---
+++ simparray.js
@@ -0,0 +1,13 @@
+var myArr = ["A", "B", "C"];
+
+//using array constryuctor
+var myArrPro = new Array("D","E", "F");
+
+console.log(myArr[1]);
+
+for(let item of myArr){
+    console.log(item);
+}
+for(let item of myArrPro){
+    console.log(item);
+}
